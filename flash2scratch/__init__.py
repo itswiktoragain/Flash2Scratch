439f9bb01ee6b9dda44f52b85b0c5cb1d1b587b2
itswiktoragain <propalnie15@gmail.com>
2026-08-18 03:47:39 +0200

    Update package version metadata

--- flash2scratch/__init__.py
+++ flash2scratch/__init__.py
@@ -1,3 +1,3 @@
-"""Flash2Scratch: experimental AVM2/ActionScript 3 -> Scratch 3 compiler."""
+"""Flash2Scratch: experimental ActionScript 1/2/3 SWF -> Scratch 3 compiler."""
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
