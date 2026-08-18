bb4f8d766531c901ac0816972d453c62369c0600
itswiktoragain <propalnie15@gmail.com>
2026-08-18 16:01:27 +0200

    Add shared ActionScript behavior mappings

---
+++ flash2scratch/behavior_common.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+KEYS = {
+    "Keyboard.LEFT": "left arrow", "Key.LEFT": "left arrow", "37": "left arrow",
+    "Keyboard.UP": "up arrow", "Key.UP": "up arrow", "38": "up arrow",
+    "Keyboard.RIGHT": "right arrow", "Key.RIGHT": "right arrow", "39": "right arrow",
+    "Keyboard.DOWN": "down arrow", "Key.DOWN": "down arrow", "40": "down arrow",
+    "Keyboard.SPACE": "space", "Key.SPACE": "space", "32": "space",
+    "Keyboard.ENTER": "enter", "Key.ENTER": "enter", "13": "enter",
+    "Key.BACKSPACE": "backspace", "8": "backspace",
+    "Key.DELETEKEY": "delete", "46": "delete",
+}
+PROPERTY_ALIASES = {
+    "_x":"x","x":"x","_y":"y","y":"y","_rotation":"rotation","rotation":"rotation",
+    "_visible":"visible","visible":"visible","_alpha":"alpha","alpha":"alpha",
+    "_xscale":"scale","_yscale":"scale","scaleX":"scale","scaleY":"scale",
+}
